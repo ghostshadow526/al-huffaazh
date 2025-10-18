5bfee0f2188e5a17f234dbbaa815c542923bd3ce
Amin H. Nasser <aminhnasser876@gmail.com>
2025-10-18 10:14:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2007384807-e48c8",
+  "appId": "1:151314649003:web:f11c92c13a0b44ff924cee",
+  "apiKey": "AIzaSyBsxyc5V7S4z1-JBbd6SDaAIUpwAhoXa-E",
+  "authDomain": "studio-2007384807-e48c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "151314649003"
+};
